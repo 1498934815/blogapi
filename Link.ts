3041de8e44da7a0bac8ade4b9e6f3d3226ee064a
JASMIAM <69405366+1498934815@users.noreply.github.com>
2025-09-10 17:14:40 +0800

    更新 Link.ts

--- Link.ts
+++ Link.ts
@@ -2,7 +2,7 @@
     {
       "name": "静谧幻想家博客",
       "link": "https://jasmiam.top",
-      "avatar": "https://q1.qlogo.cn/g?b=qq&nk=1655466387&s=640",
+      "avatar": "https://q1.qlogo.cn/g?b=qq&nk=1498934815&s=640",
       "descr": "运气是计划之外的东西."
     }
 ]
