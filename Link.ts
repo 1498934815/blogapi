a6fc25e0bb0813b21bda4aaa12edfd527e5818e6
JASMIAM <69405366+1498934815@users.noreply.github.com>
2025-09-10 17:14:57 +0800

    更新 Link.ts

--- Link.ts
+++ Link.ts
@@ -3,6 +3,6 @@
       "name": "静谧幻想家博客",
       "link": "https://jasmiam.top",
       "avatar": "https://q1.qlogo.cn/g?b=qq&nk=1498934815&s=640",
-      "descr": "运气是计划之外的东西."
+      "descr": "除了死亡，一切失败都只存在于心理层面."
     }
 ]
