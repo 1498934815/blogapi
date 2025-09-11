c37be811082272c477b459bcec5e4c7230333edb
JASMIAM <69405366+1498934815@users.noreply.github.com>
2025-09-11 18:43:21 +0800

    Delete Friends2.ts

--- Friends2.ts
+++
@@ -1,9 +0,0 @@
-[
-    {
-      "title": "测试api 中使用 Lenis 增加鼠标滚动阻尼感",
-      "auther": "韩小韩博客",
-      "date": "2025-03-06",
-      "link": "https://www.vvhan.com/article/Lenis-in-Astro",
-      "content": "第二个人"
-    }
-]
